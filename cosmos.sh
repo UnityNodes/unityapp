21de45cb341f55a5dbf9bd96e7d4e0c6d5fefeea
Fearless Oleksii <143862878+CryptoManUA@users.noreply.github.com>
2024-04-23 20:14:32 +0300

    Update cosmos.sh

--- cosmos.sh
+++ cosmos.sh
@@ -1,93 +1,94 @@
 #!/bin/bash
 
 function logo() {
-  source <(curl -s https://raw.githubusercontent.com/unitynodes/scripts/main/logo.sh)
+    bash <(curl -s https://raw.githubusercontent.com/CPITMschool/Scripts/main/logo.sh)
 }
+	
 
 function printGreen {
-     echo -e "\e[1m\e[32m${1}\e[0m"
+    echo -e "\e[1m\e[32m${1}\e[0m"
 }
 
 function printRed {
-     echo -e "\e[1m\e[31m${1}\e[0m"
-
+    echo -e "\e[1m\e[31m${1}\e[0m"
+	
 }
 function printAddition {
-     echo -e "\e[4m${1}\e[0m"
+    echo -e "\e[4m${1}\e[0m"
+	
+}
+ function server() {
+ 	bash <(curl -s https://raw.githubusercontent.com/CryptoManUA/cosmos/main/server.sh)
 }
 
-  function server() {
-  bash <(curl -s https://raw.githubusercontent.com/unitynodes/cosmos/main/server.sh)
+ function menu_node() {
+ 	bash <(curl -s https://raw.githubusercontent.com/CryptoManUA/cosmos/main/node_menu.sh)
+}
+ function monitor() {
+ 	bash <(curl -s https://raw.githubusercontent.com/asapov01/Install-Grafana/main/installmonitoring.sh)
 }
 
-  function menu_node() {
-  bash <(curl -s https://raw.githubusercontent.com/unitynodes/cosmos/main/node_menu.sh)
-}
-  function monitor() {
-  bash <(curl -s https://raw.githubusercontent.com/unitynodes/scripts/main/monitoring.sh)
-}
-
-  function backup() {
-  bash <(curl -s https://raw.githubusercontent.com/unitynodes/scritps/main/backup.sh)
+ function backup() {
+ 	bash <(curl -s https://raw.githubusercontent.com/asapov01/Backup/main/backup.sh)
 }
 
 function main_menu {
-     while true; do
-clear
-         logo
-printGreen "● Main Menu:
+    while true; do
+		clear
+        logo 
+printGreen "● Головне меню:
 │    
 │
 │ ┌───┬──────────────────────────────────────┐
-├─┤ 1 │ Commands for the server (terminal)   │
+├─┤ 1 │ Команди для сервера (термінал)       │
 │ ├───┼──────────────────────────────────────┤
-├─┤ 2 │ Cosmos network projects              │
+├─┤ 2 │ Взаємодія з нодами Cosmos            │
 │ ├───┼──────────────────────────────────────┤
-├─┤ 3 │ Monitoring                           │
+├─┤ 3 │ Моніторинг                           │
 │ ├───┼──────────────────────────────────────┤
-├─┤ 4 │ Backup                               │
+├─┤ 4 │ Бекап                                │
 │ ├───┼──────────────────────────────────────┤
-└─┤ 0 │ Exit                                 │
+└─┤ 0 │ Вийти                                │
   └───┴──────────────────────────────────────┘"
-read -p "Make your choice, and enter the item number ► " choice
-         case "$choice" in
-1)
-clear
-logo
-printGreen "▼ Commands for the server (Working with the server) ▼" && sleep 1
-echo ""
-             server
-             ;;
-2)
-clear
-logo
-printGreen "▼ Interaction with Cosmos nodes ▼" && sleep 1
-echo ""
-             menu_node
-             ;;
-3)
-printGreen "▼ Setting up monitor ▼" && sleep 1
-clear
-echo ""
-             monitor
-             ;;
-4)
-printGreen "▼ Creating backup ▼" && sleep 1
-clear
-echo ""
-             backup
-             ;;
-0)
-echo "You have exited the menu."
-             break
-             ;;
-*)
-echo
-printRed "The specified item is incorrect, please try again:"
-;;
-         esac
-read -p "Press Enter to return to the main menu..."
-done
+read -p "Зробіть ваш вибір, та введіть номер пункту ► " choice
+        case "$choice" in 
+		1)	
+			clear
+			logo
+			printGreen "▼ Команди для сервера (Робота з сервером) ▼" && sleep 1
+			echo ""
+            server
+            ;;
+		2)	
+			clear
+			logo
+			printGreen "▼ Взаємодія з нодами Cosmos ▼" && sleep 1
+			echo ""
+            menu_node
+            ;;
+		3)	
+			printGreen "▼ Встановлення моніторонгу ▼" && sleep 1
+			clear
+			echo ""
+            monitor
+            ;;
+		4)	
+			printGreen "▼ Створення бекапу ▼" && sleep 1
+			clear
+			echo ""
+            backup
+            ;;	
+		0)
+			echo "Ви вийшли з меню." 
+            break
+            ;;
+		*)	
+			echo
+			printRed "Неправильно вказаний пункт, спробуйте ще раз:"
+			;;
+        esac
+		read -p "Натисніть Enter, щоб повернутись до головного меню..."
+	done	
 }
 
 main_menu
