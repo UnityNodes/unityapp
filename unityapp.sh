b75a40985773241667b70d8ba399118de8bdb43c
Fearless Oleksii <143862878+CryptoManUA@users.noreply.github.com>
2024-04-27 12:55:56 +0300

    Update unityapp.sh

--- unityapp.sh
+++ unityapp.sh
@@ -3,19 +3,19 @@
 source <(curl -s https://raw.githubusercontent.com/UnityNodes/scripts/main/utils.sh)
 
 function server {
-bash bash <(curl -s https://raw.githubusercontent.com/UnityNodes/unityapp/main/server.sh)
+bash <(curl -s https://raw.githubusercontent.com/UnityNodes/unityapp/main/server.sh)
 }
 
 function menu_cosmos {
-bash bash <(curl -s https://raw.githubusercontent.com/UnityNodes/unityapp/main/menu_cosmos.sh)
+bash <(curl -s https://raw.githubusercontent.com/UnityNodes/unityapp/main/menu_cosmos.sh)
 }
 
 function monitoring {
-bash bash <(curl -s https://raw.githubusercontent.com/UnityNodes/unityapp/main/monitoring.sh)
+bash <(curl -s https://raw.githubusercontent.com/UnityNodes/unityapp/main/monitoring.sh)
 }
 
 function validator-backup {
-bash bash <(curl -s https://raw.githubusercontent.com/UnityNodes/unityapp/main/validator-backup.sh)
+bash <(curl -s https://raw.githubusercontent.com/UnityNodes/unityapp/main/validator-backup.sh)
 }
 
 
