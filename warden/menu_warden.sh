b1ae536e119cc3cfb7e51dd12df28ab19f461193
Fearless Oleksii <143862878+CryptoManUA@users.noreply.github.com>
2024-05-25 12:36:26 +0300

    Add files via upload

---
+++ warden/menu_warden.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/UnityNodes/scripts/main/utils.sh)
+
+function one-lines {
+	bash <(curl -s https://raw.githubusercontent.com/UnityNodes/unityapp/main/warden/one-line_warden.sh)
+}
+
+function service {
+	bash <(curl -s https://raw.githubusercontent.com/UnityNodes/unityapp/main/warden/service_warden.sh)
+}
+
+function wallet {
+	bash <(curl -s https://raw.githubusercontent.com/UnityNodes/unityapp/main/warden/wallet_warden.sh)
+}
+
+function validator {
+	bash <(curl -s https://raw.githubusercontent.com/UnityNodes/unityapp/main/warden/validator_warden.sh)
+}
+
+function maintenance {
+	bash <(curl -s https://raw.githubusercontent.com/UnityNodes/unityapp/main/warden/maintenance_warden.sh)
+}
+
+function governance {
+	bash <(curl -s https://raw.githubusercontent.com/UnityNodes/unityapp/main/warden/govermance_warden.sh)
+}
+
+function port {
+	bash <(curl -s https://raw.githubusercontent.com/UnityNodes/unityapp/main/warden/port_warden.sh)
+}
+
+
+function main_menu {
+    while true; do
+		clear
+		anima
+		clear
+        logo 
+printBlue "● Menu Warden Protocol
+│
+│ ┌───┬──────────────────────────────────────┐
+├─┤ 1 │ One-line codes (Install, Update, etc)│
+│ ├───┼──────────────────────────────────────┤
+├─┤ 2 │ Service (start,stop,restart etc)     │
+│ ├───┼──────────────────────────────────────┤
+├─┤ 3 │ Wallet (balance, delegate etc)       │
+│ ├───┼──────────────────────────────────────┤
+├─┤ 4 │ Validator (create,edit etc)          │
+│ ├───┼──────────────────────────────────────┤
+├─┤ 5 │ Maintenance (peers,sync info etc)    │
+│ ├───┼──────────────────────────────────────┤
+├─┤ 6 │ Governance (vote, proposal etc)      │
+│ ├───┼──────────────────────────────────────┤
+├─┤ 7 │ Ports (change etc)                   │
+│ ├───┼──────────────────────────────────────┤
+└─┤ 0 │ Exit                                 │
+  └───┴──────────────────────────────────────┘"
+read -p "Make your choice and enter the item number: ► " choice
+        case "$choice" in 
+		1)	
+			clear
+			echo ""
+            one-lines
+            ;;
+		2)	
+			clear
+			echo ""
+            service
+            ;;
+		3)	
+			clear
+			echo ""
+           wallet
+            ;;	
+		4)	
+			clear
+			echo ""
+            validator
+            ;;
+		5)	
+			clear
+			echo ""
+            maintenance
+            ;;
+		6)	
+			clear
+			echo ""
+            governance
+            ;;
+		7)	
+			clear
+			echo ""
+            port
+            ;;								
+		0)
+			echo "You have exited the menu." 
+            break
+            ;;
+		*)	
+			echo
+			printRed "Incorrect choice. Try again..."
+			;;
+        esac
+		read -p "Press Enter to return main menu..."
+	done	
+}
+
+main_menu
---
+++ warden/one-line_warden.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+source <(curl -s https://raw.githubusercontent.com/UnityNodes/scripts/main/utils.sh)
+
+
+function main_menu {
+    while true; do
+		clear
+		anima
+		clear
+        logo 
+printBlue "● One-line codes Warden Protocol
+│
+│ ┌───┬──────────────────────────────────────┐
+├─┤ 1 │ Auto installer                       │
+│ ├───┼──────────────────────────────────────┤
+├─┤ 2 │ Auto update                          │
+│ ├───┼──────────────────────────────────────┤
+├─┤ 3 │ Remove node                          │
+│ ├───┼──────────────────────────────────────┤
+└─┤ 0 │ Exit                                 │
+  └───┴──────────────────────────────────────┘"
+read -p "Make your choice and enter the item number: ► " choice
+        case "$choice" in 
+		1)	
+			echo ""
+            printGreen "▼ Auto installer ▼"
+			
+			;;
+		2)	
+			echo ""
+            printGreen "▼ Auto update ▼"
+			printRed "There are no actual updates"
+			;;
+		3)	
+			echo ""
+            printGreen "▼ Remove node ▼"
+            echo ""
+			cd $HOME
+			sudo systemctl stop wardend
+			sudo systemctl disable wardend
+			sudo rm /etc/systemd/system/warden.service
+			sudo systemctl daemon-reload
+			rm -f $(which wardend)
+			rm -rf ~/.warden
+			rm -rf $HOME/wardenprotocol
+            echo ""	
+			;;
+		0)
+			echo ""
+			echo "You have exited the menu." 
+            break
+            ;;
+		*)	
+			echo
+			printRed "Incorrect choice. Try again..."
+			;;
+        esac
+		read -p "Press Enter to return main menu..."
+	done	
+}
+
+main_menu
