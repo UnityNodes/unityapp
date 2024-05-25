42f830b6d3fa50a6fe0430c55a1e927d5cd12487
Fearless Oleksii <143862878+CryptoManUA@users.noreply.github.com>
2024-05-25 12:55:38 +0300

    Update menu_cosmos.sh

--- menu_cosmos.sh
+++ menu_cosmos.sh
@@ -14,25 +14,10 @@ function menu_side {
 	bash <(curl -s https://raw.githubusercontent.com/UnityNodes/unityapp/main/side/menu_side.sh)
 }
 
-function menu_lava {
-	bash <(curl -s https://raw.githubusercontent.com/UnityNodes/cosmos/lava/menu_lava.sh)
+function menu_initia {
+	bash <(curl -s https://raw.githubusercontent.com/UnityNodes/unityapp/main/initia/menu_initia.sh)
 }
 
-function menu_babylon {
-	bash <(curl -s https://raw.githubusercontent.com/UnityNodes/cosmos/babylon/menu_babylon.sh)
-}
-
-function menu_cascadia {
-	bash <(curl -s https://raw.githubusercontent.com/UnityNodes/cosmos/cascadia/menu_cascadia.sh)
-}
-
-function menu_dymension {
-	bash <(curl -s https://raw.githubusercontent.com/UnityNodes/cosmos/dymension/menu_dymension.sh)
-}
-
-function menu_zeta {
-	bash <(curl -s https://raw.githubusercontent.com/UnityNodes/cosmos/zetachain/menu_zeta.sh)
-}
 
 function main_menu {
     while true; do
@@ -49,15 +34,7 @@ printBlue "● Cosmos menu:
 │ ├───┼──────────────────────────────────────┤
 ├─┤ 3 │ Side Protocol                        │
 │ ├───┼──────────────────────────────────────┤
-├─┤ 4 │ Lava Network                         │
-│ ├───┼──────────────────────────────────────┤
-├─┤ 5 │ Babylon                              │
-│ ├───┼──────────────────────────────────────┤
-├─┤ 6 │ Cascadia                             │
-│ ├───┼──────────────────────────────────────┤
-├─┤ 7 │ Dymension                            │
-│ ├───┼──────────────────────────────────────┤
-├─┤ 8 │ ZetaChain                            │
+├─┤ 4 │ Initia                               │
 │ ├───┼──────────────────────────────────────┤
 └─┤ 0 │ Exit                                 │
   └───┴──────────────────────────────────────┘"
@@ -74,21 +51,8 @@ read -p "Make your choice and enter the item number: ► " choice
 			menu_side
 			;;
 		4)	
-			menu_lava
-			;;
-
-		5)	
-			menu_babylon
-			;;
-		6)	
-			menu_cascadia
-			;;
-		7)	
-			menu_dymension
-			;;
-		8)	
-			menu_zeta
-			;;		
+			menu_initia
+			;;	
 		0)
 			echo ""
 			echo "You have exited the menu." 
